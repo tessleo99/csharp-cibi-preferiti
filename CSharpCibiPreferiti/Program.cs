b6ac04ebca14152dc7e6350bec8e605da1ce5d19
tessleo99 <t.napoletano9@gmail.com>
2023-04-14 15:05:18 +0200

    Aggiungere i file di progetto.

---
+++ CSharpCibiPreferiti/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ Nel programma inizializzate un array con la classifica dei vostri cibi preferiti (minimo 5, massimo 10 elementi).
+Una volta dichiarato l’array di cibi preferiti, stampate a video le seguenti informazioni:
+La lunghezza della classifica
+La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
+Il vostro cibo top (prima posizione della classifica)
+Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
+Cercate per ogni richiesta che viene fatta di preparare le rispettive funzioni  e di usarle nel vostro programma principale!
+ */
+
+//MAIN PROGRAM
+//PASSO 1 : dichiaro l'array
+
+string[] favouriteFoods = { "pizza", "carbonara", "parmigiana", "spezzatino", "alici" };
+
+//PASSO 2 : stampo la lunghezza della mia classifica
+Console.Write("La lunghezza della classifica è: " + favouriteFoods.Length);
+
+
+
+//FUNCTIONS
